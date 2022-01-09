1077861b5bc8e4b1b5c852dd8481094438ac95c0
Melissa Tro <s0571267@htw-berlin.de>
2022-01-09 22:03:59 +0100

    Add tests

--- src/test/java/htw/berlin/webtech/demo/WebtechApplicationTests.java
+++ src/test/java/htw/berlin/webtech/demo/WebtechApplicationTests.java
@@ -1,13 +1,36 @@
 package htw.berlin.webtech.demo;
 
-import org.junit.jupiter.api.Test;
+import htw.berlin.webtech.persistence.PollEntity;
+import htw.berlin.webtech.persistence.PollRepository;
+import htw.berlin.webtech.web.api.Poll;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import htw.berlin.webtech.service.PollService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 @SpringBootTest
 class WebtechApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@DisplayName("should test if poll  creation was successful")
+	void pollCreationTest(){
+		Poll poll = new Poll(1, "Wie spät ist es?", "Hans", "2021-01-01", "2021-01-03", "1Uhr,2Uhr,3Uhr", "0,1,1");
+		assertNotNull(poll);
+		assertEquals(poll.getId(), 1);
+	}
+
+
+	@Test
+	@DisplayName("should test if setter method for username is working")
+	void setUsernameTest(){
+		Poll poll = new Poll(1, "Wie spät ist es?", "Hans", "2021-01-01", "2021-01-03", "1Uhr,2Uhr,3Uhr", "0,1,1");
+		poll.setUsername("Hannah");
+		assertEquals("Hannah", poll.getUsername());
+
+
 	}
 
 }
